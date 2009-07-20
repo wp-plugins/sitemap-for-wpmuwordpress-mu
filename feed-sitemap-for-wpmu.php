69846bfedd5a4f0541ab8ebd3ab907ead351c2ff
followta.com <followta.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-07-20 13:22:28 +0000

    tagging version 1.0
    git-svn-id: https://plugins.svn.wordpress.org/sitemap-for-wpmuwordpress-mu/tags/1.0@136995 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ feed-sitemap-for-wpmu.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * XML Sitemap Feed Template for WPMU to display XML Sitemap Posts feed.
+ * Official website: http://followta.com
+ * WPMU version: 2.8.1
+ * @package followta.com/Wordpress
+ */
+
+header('Content-Type: text/xml; charset=' . get_option('blog_charset'), true);
+$more = 1;
+
+?>
+<?php 
+
+	echo '<?xml version="1.0" encoding="'.get_option('blog_charset').'"?'.'>'; 
+
+        $sitemapstyle = ABSPATH. "wp-content/mu-plugins/sitemap4wpmu.xsl";
+	if(file_exists($sitemapstyle)) {
+		$url = get_option('siteurl'). "/wp-content/mu-plugins/sitemap4wpmu.xsl";
+		echo '<?xml-stylesheet type="text/xsl" href="'. $url. '"?>';
+	}
+?>
+
+<!-- generator="http://followta.com" -->
+<urlset	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+	xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
+	xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+>
+
+<?php 
+
+        $blogs = $wpdb->get_results("SELECT blog_id, domain, path, last_updated FROM $wpdb->blogs WHERE
+                public = '1' ORDER BY last_updated DESC");
+
+        if ($blogs) {
+		foreach ($blogs as $blog) {
+			$blogPostsTable = "wp_".$blog->blog_id."_posts";
+			echo "<url><loc>http://". $blog->domain. $blog->path. "</loc>";
+			echo "<lastmod>". mysql2date('Y-m-d\TH:i:s+00:00', $blog->last_updated, false). "</lastmod>";
+			echo "<changefreq>weekly</changefreq>";
+			echo "<priority>0.5</priority>";
+			echo "</url>"; 
+
+			$post_ids = $wpdb->get_col("SELECT ID FROM $blogPostsTable WHERE post_status = 'publish' ORDER BY post_date_gmt DESC LIMIT 1000");
+
+			if ($post_ids) {
+				global $wp_query;
+				$wp_query->in_the_loop = true;
+
+				while ( $next_posts = array_splice($post_ids, 0, 20) ) {
+					$where = "WHERE ID IN (".join(',', $next_posts).")";
+					$query_jiang = "SELECT * FROM $blogPostsTable WHERE post_status = 'publish' 
+						AND post_type = 'post' ORDER BY post_date_gmt DESC";
+					$posts = $wpdb->get_results($query_jiang);
+					foreach ($posts as $post) {
+						setup_postdata($post); 
+?>
+	<url><loc><?php echo get_blog_permalink($blog->blog_id, $post->ID); ?></loc>
+	<lastmod><?php echo mysql2date('Y-m-d\TH:i:s+00:00', get_post_time('Y-m-d H:i:s', true), false); ?></lastmod>
+	<changefreq>weekly</changefreq>
+	<priority>0.6</priority>
+	</url>
+	
+<?php } } } } } ?> 
+
+</urlset>
